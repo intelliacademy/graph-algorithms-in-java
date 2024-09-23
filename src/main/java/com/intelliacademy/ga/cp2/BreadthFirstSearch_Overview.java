74e854e6fb8595d27d2b01f602626fcd8c2eae61
Vugar Mammadli <mammadli.vugar@hotmail.com>
2024-09-23 23:08:30 +0200

    first look

--- src/main/java/com/intelliacademy/ga/cp2/BreadthFirstSearch_Overview.java
+++ src/main/java/com/intelliacademy/ga/cp2/BreadthFirstSearch_Overview.java
@@ -11,17 +11,34 @@ public class BreadthFirstSearch_Overview {
         Vertex vertexC = Vertex.of("C");
         Vertex vertexD = Vertex.of("D");
         Vertex vertexE = Vertex.of("E");
+        Vertex vertexF = Vertex.of("F");
+        Vertex vertexG = Vertex.of("G");
+        Vertex vertexH = Vertex.of("H");
+        Vertex vertexI = Vertex.of("I");
+        Vertex vertexJ = Vertex.of("J");
+        Vertex vertexK = Vertex.of("K");
+        Vertex vertexL = Vertex.of("L");
 
         rootVertex.addAdjacentVertex(vertexB);
         rootVertex.addAdjacentVertex(vertexC);
 
-        vertexB.addAdjacentVertex(rootVertex);
+        vertexB.addAdjacentVertex(vertexD);
 
-        vertexC.addAdjacentVertex(rootVertex);
+        vertexC.addAdjacentVertex(vertexD);
 
         vertexD.addAdjacentVertex(vertexE);
+        vertexD.addAdjacentVertex(vertexF);
 
         vertexE.addAdjacentVertex(vertexD);
+
+        vertexF.addAdjacentVertex(vertexG);
+        vertexF.addAdjacentVertex(vertexH);
+        vertexF.addAdjacentVertex(vertexI);
+
+        vertexG.addAdjacentVertex(vertexF);
+        vertexG.addAdjacentVertex(vertexJ);
+        vertexG.addAdjacentVertex(vertexK);
+
 
         BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch(rootVertex);
 
