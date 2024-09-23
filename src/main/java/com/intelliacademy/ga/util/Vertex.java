3c32df5f8c07cc424676089b167a6ae1993ce092
Vugar Mammadli <mammadli.vugar@hotmail.com>
2024-09-23 23:03:21 +0200

    first look

--- src/main/java/com/intelliacademy/ga/util/Vertex.java
+++ src/main/java/com/intelliacademy/ga/util/Vertex.java
@@ -2,20 +2,29 @@ package com.intelliacademy.ga.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public record Vertex(String name, Boolean visited, List<Vertex> adjacentVertices) {
-    public Vertex {
+public final class Vertex {
+    private final String name;
+    private Boolean visited;
+    private final List<Vertex> adjacentVertices;
+
+    public Vertex(String name, Boolean visited, List<Vertex> adjacentVertices) {
         if (name == null) {
             throw new IllegalArgumentException("Vertex name cannot be null");
         }
+        this.name = name;
+        this.visited = visited;
+        this.adjacentVertices = adjacentVertices;
     }
 
     public static Vertex of(String name) {
-        return new Vertex(name,false, new ArrayList<>());
+        return new Vertex(name, false, new ArrayList<>());
     }
 
     public Vertex visit() {
-        return new Vertex(name, true, adjacentVertices);
+        visited = true;
+        return this;
     }
 
     public Boolean hasUnvisitedAdjacentVertex() {
@@ -40,4 +49,40 @@ public record Vertex(String name, Boolean visited, List<Vertex> adjacentVertices
         }
         System.out.println();
     }
+
+    public String name() {
+        return name;
+    }
+
+    public Boolean visited() {
+        return visited;
+    }
+
+    public List<Vertex> adjacentVertices() {
+        return adjacentVertices;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Vertex) obj;
+        return Objects.equals(this.name, that.name) &&
+                Objects.equals(this.visited, that.visited) &&
+                Objects.equals(this.adjacentVertices, that.adjacentVertices);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, visited, adjacentVertices);
+    }
+
+    @Override
+    public String toString() {
+        return "Vertex[" +
+                "name=" + name + ", " +
+                "visited=" + visited + ", " +
+                "adjacentVertices=" + adjacentVertices + ']';
+    }
+
 }
