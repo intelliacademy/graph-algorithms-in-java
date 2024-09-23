80f9abd8c3e12b6fd1127b0e5a8cf664b4e678f9
Vugar Mammadli <mammadli.vugar@hotmail.com>
2024-09-24 00:03:06 +0200

    BFS as WebCrawler

--- src/main/java/com/intelliacademy/ga/cp3/WebCrawler.java
+++ src/main/java/com/intelliacademy/ga/cp3/WebCrawler.java
@@ -1,7 +1,52 @@
 package com.intelliacademy.ga.cp3;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class WebCrawler {
+    private final Queue<String> queue;
+    private final List<String> discoveredWebsites;
+
+    public WebCrawler() {
+        this.queue = new LinkedList<>();
+        this.discoveredWebsites = new ArrayList<>();
+    }
+
+
     public void discoverWeb(String root) {
-        // Some code here
+        this.queue.add(root);
+        this.discoveredWebsites.add(root);
+
+        while (!queue.isEmpty()){
+            var v = this.queue.remove();
+            var rawHtml = readUrl(v);
+            var pattern = Pattern.compile("https://(\\w+\\.)*(\\w+)");
+            Matcher matcher = pattern.matcher(rawHtml);
+            while (matcher.find()){
+                var url = matcher.group();
+                if (!discoveredWebsites.contains(url)){
+                    discoveredWebsites.add(url);
+                    System.out.println("Website found with URL: " + url);
+                    queue.add(url);
+                }
+            }
+        }
+    }
+
+    private String readUrl(String url) {
+        var rawHtml = new StringBuilder();
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL(url).openStream()));) {
+            String inputLine = "";
+            while ((inputLine = bufferedReader.readLine()) != null){
+                rawHtml.append(inputLine);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return rawHtml.toString();
     }
 }
--- src/main/java/com/intelliacademy/ga/cp3/WebCrawler_Overview.java
+++ src/main/java/com/intelliacademy/ga/cp3/WebCrawler_Overview.java
@@ -3,6 +3,6 @@ package com.intelliacademy.ga.cp3;
 public class WebCrawler_Overview {
     public static void main(String[] args) {
         WebCrawler webCrawler = new WebCrawler();
-        webCrawler.discoverWeb("http://www.bbc.com");
+        webCrawler.discoverWeb("https://www.bbc.com");
     }
 }
