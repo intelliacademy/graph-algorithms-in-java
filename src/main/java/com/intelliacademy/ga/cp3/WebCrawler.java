e8b2e922eba66f80f505eb34b6662eff8393cbe9
Vugar Mammadli <mammadli.vugar@hotmail.com>
2024-09-23 23:23:18 +0200

    BFS as WebCrawler

---
+++ src/main/java/com/intelliacademy/ga/cp3/WebCrawler.java
@@ -0,0 +1,7 @@
+package com.intelliacademy.ga.cp3;
+
+public class WebCrawler {
+    public void discoverWeb(String root) {
+        // Some code here
+    }
+}
---
+++ src/main/java/com/intelliacademy/ga/cp3/WebCrawler_Overview.java
@@ -0,0 +1,8 @@
+package com.intelliacademy.ga.cp3;
+
+public class WebCrawler_Overview {
+    public static void main(String[] args) {
+        WebCrawler webCrawler = new WebCrawler();
+        webCrawler.discoverWeb("http://www.bbc.com");
+    }
+}
